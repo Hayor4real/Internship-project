b7e465ab717de69f5aa20f1fcc64136a97b69ce1
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2024-07-09 20:22:31 +0200

    Add form fields for Title, Price, Stock Quantity, and Description

--- Shopify Polaris/Shopify-Polaris/shopify-polaris-mobx/src/components/ProductForm.jsx
+++ Shopify Polaris/Shopify-Polaris/shopify-polaris-mobx/src/components/ProductForm.jsx
@@ -11,9 +11,42 @@ import {
 } from "@shopify/polaris";
 import { useStore } from "../stores/ProductStore";
 
+// ProductForm component for creating a new product
 const ProductForm = observer(() => {
   const store = useStore();
-  return <div>ProductForm</div>;
+
+  return (
+    <Page title="Create Product">
+      <Card sectioned>
+        <Form>
+          <FormLayout>
+            <TextField label="Title" value={store.title} autoComplete="off" />
+            <TextField
+              label="Price"
+              value={store.price}
+              autoComplete="off"
+              type="number"
+            />
+            <TextField
+              label="Stock Quantity"
+              value={store.stockQuantity}
+              autoComplete="off"
+              type="number"
+            />
+            <TextField
+              label="Description"
+              value={store.description}
+              autoComplete="off"
+              multiline={4}
+            />
+            <Button primary submit>
+              Create Product
+            </Button>
+          </FormLayout>
+        </Form>
+      </Card>
+    </Page>
+  );
 });
 
 export default ProductForm;
