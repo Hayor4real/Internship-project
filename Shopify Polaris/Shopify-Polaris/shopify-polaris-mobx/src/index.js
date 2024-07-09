c154e4b5fa6dc2d847c62b93035d0abd6a749600
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2024-07-09 19:17:00 +0200

    Import AppProvider and Polaris styles:

--- Shopify Polaris/Shopify-Polaris/shopify-polaris-mobx/src/index.js
+++ Shopify Polaris/Shopify-Polaris/shopify-polaris-mobx/src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import { AppProvider } from "@shopify/polaris";
+import "@shopify/polaris/build/esm/styles.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
 
